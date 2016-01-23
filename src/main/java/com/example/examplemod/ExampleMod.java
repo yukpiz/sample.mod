0c5b189701f3d4dec53a42ec233486c1615a886c
yukpiz <yukpiz@gmail.com>
2016-01-24 03:14:36 +0900

    Updated.

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -4,18 +4,20 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.event.entity.player.PlayerDropsEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
+    public static final String MODID = "unlost";
+    public static final String VERSION = "0.1";
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
+        // some example code
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
     }
 
